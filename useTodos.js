08536587c62f39920509b6d35e1e6df7bcb5df44
Stefania Ferreira <stefania.ferreira@sinco.com.co>
2022-09-01 11:40:08 -0500

    Primer Commit

---
+++ useTodos.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = () => {
+
+   const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+   useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos])
+
+   const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add todo',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const  handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] remove todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => { 
+        dispatch({
+            type: '[TODO] toggle todo',
+            payload: id
+        })
+    } 
+
+    return{
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=> !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,   
+        handleToggleTodo,
+    }
+}
